94c13de86c0e86c8a7a26fd07ef6090a03c0b549
Vikitoria007 <toryrovaa@gmail.com>
2024-12-10 11:06:47 +0300

    Добавлен ввод элементов массива пользователем

--- max_finder.py
+++ max_finder.py
@@ -2,4 +2,12 @@ def find_max(numbers):
     if not numbers:
         return None
     return max(numbers)
+if __name__ == "__main__":
+    count = int(input("Введите количество элементов: "))
+    numbers = []
+    for _ in range(count):
+        num = float(input("Введите число: "))
+        numbers.append(num)
+    
+    print("Максимум:", find_max(numbers))
 
